c17b6bad1f16a63022951d23f60edc379c34d450
unknown <meshramsourav282@gmail.com>
2025-04-22 23:51:22 +0530

    sql first task

---
+++ insertAndSelectPaint.sql
@@ -0,0 +1,524 @@
+create table sale (
+ id int primary key,
+ paint_type varchar,
+ price float,
+ lit int,
+ margin double PRECISION,
+ rating float,
+ coverage_sq_ft int,
+ sell_date date
+)
+
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (1,'spray',215,2,5,2.5,3,'21-04-25')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (2,'oil',321,5,3,5,6,'07-09-25')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date) 
+values (3,'oil',121,5,3,6,'07-11-25')
+
+select * from sale 
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date)
+values (4,'spray',151,1,4,3,'01-05-19')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date)
+values (5,'spray',264,7,6,1,'29-12-22')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date)
+values (6,'spray',641,4,6,3,8,'13-12-15')
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date)
+values (7,'spray',789,1,6,4,'20-02-23')
+
+select * from sale 
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date)
+values (8,'spray',531,5,2,5,'15-3-21')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft)
+values (9,'spray',216,2,4,6.1,3)
+
+insert into sale (id,paint_type,price,lit,coverage_sq_ft,sell_date)
+values (10,'spray',569,3,7,'22-05-21')
+
+select * from sale
+
+insert into sale (id,paint_type,price,margin,rating,sell_date)
+values (11,'spray',156,2,6,'15-11-19')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date)
+values (12,'spray',156,2,1,4.5,7,'03-10-18')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date)
+values (13,'oil',764,5,4,9,7,'06-07-18')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft)
+values (14,'oil',951,8,3,2,3)
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date)
+values (15,'oil',753,6,3,1,'01-10-20')
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date)
+values (16,'oil',426,7,6,1.2,'17-11-18')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date)
+values (17,'oil',8,3,1,3,'15-04-21')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date)
+values (18,'oil',456,4,3,4,'21-07-22')
+
+insert into sale (id,paint_type,price,margin,rating,sell_date)
+values (19,'oil',198,3,4,'26-03-21')
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date)
+values (20,'oil',712,3,4,3,'14-11-23')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (21,'spray',305,3,4,3.1,5,'11-06-20')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (22,'oil',489,6,2,4.4,6,'25-09-22')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date) 
+values (23,'spray',174,4,2,3,'01-08-21')
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date) 
+values (24,'spray',239,2,4.2,4,'13-03-23')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date) 
+values (25,'oil',565,5,5,4,'19-12-19')
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date) 
+values (26,'oil',711,4,3,6,'27-10-22')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date) 
+values (27,'spray',219,1,3,2,'07-05-20')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft) 
+values (28,'oil',390,7,2,4.7,5)
+
+insert into sale (id,paint_type,price,lit,coverage_sq_ft,sell_date) 
+values (29,'spray',141,2,4,'08-04-24')
+
+insert into sale (id,paint_type,price,margin,rating,sell_date) 
+values (30,'spray',333,3,4,'03-07-21')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (31,'oil',614,8,5,4.9,7,'14-01-20')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (32,'spray',213,3,2,3.6,2,'05-02-23')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date) 
+values (33,'oil',875,6,4,4,'10-10-23')
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date) 
+values (34,'spray',129,2,3,2,'18-08-20')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date) 
+values (35,'oil',564,5,2,6,'29-05-22')
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date) 
+values (36,'spray',288,4,3.5,3,'12-06-21')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date) 
+values (37,'oil',723,9,4,8,'03-03-25')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (38,'spray',346,2,1,2.8,2,'24-01-24')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date) 
+values (39,'oil',675,5,5,4,'06-02-23')
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date) 
+values (40,'spray',389,2,4,4,'02-12-20')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (41,'plastic',215,2,3,3.8,4,'04-03-21')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date) 
+values (42,'plastic',278,2,3.5,4,'15-12-21')
+
+insert into sale (id,paint_type,price,lit,margin,sell_date) 
+values (43,'plastic',267,3,4,'18-05-23')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date) 
+values (44,'plastic',205,2,1,3.2,'08-02-21')
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date) 
+values (45,'plastic',188,2,2,3,'25-04-21')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (46,'plastic',234,1,3,2,3,'23-09-22')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (47,'acrylic',452,5,4,4.6,6,'22-08-22')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date) 
+values (48,'acrylic',598,6,5,6,'30-09-20')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date) 
+values (49,'acrylic',560,5,4,6,'21-09-23')
+
+insert into sale (id,paint_type,price,lit,margin,sell_date) 
+values (50,'acrylic',429,2,3,'26-03-21')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (51,'acrylic',339,4,3,4.5,5,'13-03-24')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date) 
+values (52,'acrylic',478,3,4,6,'12-10-23')
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date) 
+values (53,'exterior',389,3,2,5,'11-10-23')
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date) 
+values (54,'exterior',419,5,3,7,'14-06-22')
+
+select * from sale 
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date) 
+values (55,'exterior',499,6,5,4.9,'12-11-20')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date) 
+values (56,'exterior',460,5,4,4.3,'05-10-22')
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date) 
+values (57,'exterior',367,4,3.3,5,'30-07-22')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (58,'exterior',442,5,4,4.9,7,'13-10-23')
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date) 
+values (59,'deco',314,4,4.0,3,'03-04-24')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date) 
+values (60,'deco',341,3,3,4.4,'09-01-22')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (61,'deco',599,6,5,5.4,7,'16-08-23')
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date) 
+values (62,'deco',410,4,3.9,4,'16-07-22')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date) 
+values (63,'deco',628,6,4,5.8,'18-01-24')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date) 
+values (64,'deco',575,5,4,7,'08-06-23')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (65,'spray',275,3,4,4.2,5,'15-03-24')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (66,'oil',432,6,3.5,4.8,6,'22-11-23')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (67,'deco',512,5,4.2,5.5,7,'10-09-22')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (68,'acrylic',598,4,3.8,4.9,6,'18-08-21')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (69,'exterior',654,7,4.5,4.7,8,'25-06-20')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (70,'plastic',289,3,3.2,4.1,5,'12-04-19')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date)
+values (71,'plastic',245,4,4.5,4.2,6,'10-03-23')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date)
+values (72,'plastic',260,5,3.8,4.0,7,'15-06-22')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date)
+values (73,'plastic',220,2,4.2,3.9,5,'20-09-21')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date)
+values (74,'plastic',235,3,4.0,4.1,6,'05-12-20')
+
+select * from sale
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date)
+values (75,'spray',205,3,3.5,5,'15-06-23')
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date)
+values (76,'spray',489,6,4.5,7,'05-12-21')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date)
+values (77,'spray',580,7,4.3,8,'22-04-20')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date)
+values (78,'spray',610,8,4.5,4.8,'30-01-19')
+
+select * from sale 
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date)
+values (79,'oil',700,3.2,4.6,6,'12-03-24')
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date)
+values (80,'oil',720,6,4.7,7,'15-05-23')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date)
+values (81,'oil',850,7,3.8,8,'18-09-22')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date)
+values (82,'oil',780,8,4.0,5.0,'25-02-21')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft)
+values (83,'oil',900,9,4.2,5.2,10)
+
+select * from sale 
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date)
+values (84,'plastic',250,3.6,4.0,5,'12-07-24')
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date)
+values (85,'plastic',275,3,4.2,6,'20-10-23')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date)
+values (86,'plastic',295,4,4.0,7,'15-03-22')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date)
+values (87,'plastic',320,5,4.3,4.7,'05-06-21')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft)
+values (88,'plastic',340,6,4.5,4.9,9)
+
+select * from sale 
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date)
+values (89,'acrylic',550,4.2,4.8,6,'14-07-23')
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date)
+values (90,'acrylic',600,8,5.9,7,'12-10-22')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date)
+values (91,'acrylic',630,9,4.8,8,'20-02-21')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date)
+values (92,'acrylic',660,10,9.1,9,'01-03-20')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft)
+values (93,'acrylic',690,11,5.2,5.5,10)
+
+select * from sale
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date)
+values (94,'deco',430,4.0,4.3,6,'05-02-25')
+
+insert into sale (id,paint_type,price,lit,rating,coverage_sq_ft,sell_date)
+values (95,'deco',460,6,4.5,7,'07-08-24')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft,sell_date)
+values (96,'deco',490,7,4.5,8,'15-12-23')
+
+insert into sale (id,paint_type,price,lit,margin,rating,sell_date)
+values (97,'deco',520,8,4.7,5,'18-05-22')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft)
+values (98,'deco',550,9,4.9,5.2,10)
+
+select * from sale 
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date)
+values (99,'oil',710,10,3.5,7.4,11,'03-01-20')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date)
+values (100,'oil',740,11,8.2,6.5,12,'06-11-19')
+
+select * from sale 
+
+insert into sale (id,paint_type,price,margin,rating,coverage_sq_ft,sell_date) 
+values (101,'oil',200,4.5,4.2,300,'01-04-25')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (102,'spray',150,2,3.2,3.8,200,'02-04-20')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (103,'acrylic',220,4,5.1,4.7,250,'03-04-19')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (104,'plastic',180,3,4.5,4.1,180,'04-04-25')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (105,'deco',300,6,4.8,4.5,50,'05-04-25')
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft,sell_date) 
+values (106,'exterior',250,7,4.3,4.2,400,'06-04-25')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft) 
+values (107,'spray',215,2,3.6,4.0,6)
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft) 
+values (108,'oil',340,4,2.8,4.5,7)
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft) 
+values (109,'acrylic',480,5,3.9,4.7,8)
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft) 
+values (110,'plastic',200,3,3.5,4.1,5)
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft) 
+values (111,'deco',350,4,4.0,5.0,6)
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft) 
+values (112,'exterior',410,5,4.1,5.3,8)
+
+select * from sale
+
+insert into sale (id,paint_type,price) 
+values (113,'exterior',220)
+
+insert into sale (id,paint_type,price,lit,rating) 
+values (114,'oil',300,3,4.2)
+
+insert into sale (id,paint_type,price,margin,coverage_sq_ft) 
+values (115,'spray',185,3.1,6)
+
+insert into sale (id,paint_type,price,lit,margin) 
+values (116,'acrylic',390,4,2.9)
+
+insert into sale (id,paint_type,price,rating) 
+values (117,'plastic',275,4.6)
+
+insert into sale (id,paint_type,price,coverage_sq_ft,sell_date) 
+values (118,'exterior',450,9,'05-11-25')
+
+insert into sale (id,paint_type,price,margin,coverage_sq_ft) 
+values (119,'deco',330,4.0,7)
+
+insert into sale (id,paint_type,price,lit,margin,rating) 
+values (120,'spray',250,3,3.5,4.0)
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,rating) 
+values (121,'exterior',280,2,4.0)
+
+insert into sale (id,paint_type,price,margin,coverage_sq_ft) 
+values (122,'oil',320,3.2,5)
+
+insert into sale (id,paint_type,price,rating,coverage_sq_ft) 
+values (123,'acrylic',275,4.3,8)
+
+insert into sale (id,paint_type,price,lit,margin,rating) 
+values (124,'spray',250,3,3.5,4.2)
+
+insert into sale (id,paint_type,price,coverage_sq_ft,sell_date) 
+values (125,'plastic',210,6,'05-12-25')
+
+insert into sale (id,paint_type,price,lit,margin) 
+values (126,'exterior',350,5,2.7)
+
+insert into sale (id,paint_type,price,rating) 
+values (127,'deco',300,4.5)
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft) 
+values (128,'exterior',260,4,3.1,4.0,7)
+
+insert into sale (id,paint_type,price,margin,coverage_sq_ft) 
+values (129,'oil',330,3.0,8)
+
+insert into sale (id,paint_type,price,lit,sell_date) 
+values (130,'acrylic',290,3,'05-12-25')
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft) 
+values (131,'exterior',275,2.5,3.4,9)
+
+insert into sale (id,paint_type,price,rating,coverage_sq_ft) 
+values (132,'oil',310,4.1,7)
+
+insert into sale (id,paint_type,price,margin,rating) 
+values (133,'acrylic',250,2.9,4.0)
+
+insert into sale (id,paint_type,price,lit,sell_date) 
+values (134,'spray',220,2,'05-14-25')
+
+insert into sale (id,paint_type,price,margin,coverage_sq_ft) 
+values (135,'plastic',230,3.1,5)
+
+insert into sale (id,paint_type,price,rating,coverage_sq_ft) 
+values (136,'exterior',330,4.2,6)
+
+insert into sale (id,paint_type,price,lit,margin) 
+values (137,'deco',260,3.2,3.0)
+
+insert into sale (id, paint_type,price,rating,sell_date) 
+values (138,'exterior',280,4.4,'05-10-25')
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft) 
+values (139,'oil',315,4,2.8,10)
+
+insert into sale (id,paint_type,price,rating,coverage_sq_ft) 
+values (140,'acrylic',295,4.0,8)
+
+select * from sale
+
+insert into sale (id,paint_type,price,lit,margin,rating,coverage_sq_ft) 
+values (141,'exterior',320,3.6,3.2,4.3,8)
+
+insert into sale (id,paint_type,price,rating,coverage_sq_ft) 
+values (142,'oil',290,4.1,7)
+
+insert into sale (id,paint_type,price,margin,coverage_sq_ft) 
+values (143,'acrylic',275,3.0,9)
+
+insert into sale (id,paint_type,price,lit,margin) 
+values (144,'spray',245,2.8,3.5)
+
+insert into sale (id,paint_type,price,rating,coverage_sq_ft) 
+values (145,'plastic',210,4.0,5)
+
+insert into sale (id,paint_type,price,margin,rating) 
+values (146,'exterior',330,3.4,4.5)
+
+insert into sale (id,paint_type,price,lit,sell_date) 
+values (147,'deco',255,3.2,'05-03-25')
+
+insert into sale (id,paint_type,price,margin,coverage_sq_ft) 
+values (148,'exterior',300,3.8,7)
+
+insert into sale (id,paint_type,price,rating,coverage_sq_ft) 
+values (149,'oil',310,4.2,6)
+
+insert into sale (id,paint_type,price,lit,margin,coverage_sq_ft) 
+values (150,'acrylic',270,2.7,3.0,10)
+
+select * from sale
+
+
+
